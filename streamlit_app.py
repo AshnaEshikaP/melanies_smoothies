1b0c960b37d62f32a1fa6cdd5a49743c07655a69
AshnaEshikaP <166601255+AshnaEshikaP@users.noreply.github.com>
2024-10-23 11:17:02 +0530

    Update streamlit_app.py
    some stupid changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,16 @@
 # Import python packages
 import streamlit as st
-
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
-streamlit.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
-streamlit.write(
+st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
+st.write(
     """Choose the fruits you want in your custom Smoothie .
     """
 )
 
 
-from snowflake.snowpark.functions import col
+
 
 cnx= st.connection("snowflake")
 session=cnx.session()
